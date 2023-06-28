ede5967bdad9f5283be745c2fd2541752a18fb4f
Mikhail Fedorov <mfedorofff@mail.ru>
2023-06-28 15:01:23 +0300

    Create branch "layout", create splash-screen, create Profile fragment. Add dependency, and clean code with lint, ktlint, SonarLint.

--- app/src/main/java/com/github/mishkaff89/practicumdev/MainActivity.kt
+++ app/src/main/java/com/github/mishkaff89/practicumdev/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.github.mishkaff89.practicumdev
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.mishkaff89.practicumdev.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navigationSetup()
+        supportFragmentManager.beginTransaction().add(R.id.fragmentContainerView, ProfileFragment())
+            .commit()
+    }
+
+    private fun navigationSetup() {
+        binding.bottomNavigationView.selectedItemId = R.id.nav_profile
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_news -> {
+                    true
+                }
+
+                R.id.nav_search -> {
+                    true
+                }
+
+                R.id.nav_help -> {
+                    true
+                }
+
+                R.id.nav_history -> {
+                    true
+                }
+
+                R.id.nav_profile -> {
+                    true
+                }
+
+                else -> false
+            }
+        }
     }
 }
